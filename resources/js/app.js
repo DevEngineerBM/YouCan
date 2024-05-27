6b51a9cbc3e36b7668bbe63dbb114bdb4f98772f
belkhalil <belkhalil1mohamed@gmail.com>
2024-05-27 20:29:35 +0100

    MIXED: Upadte app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './Router' 
+
+
+const app = createApp(App)
+  .use(router)
+  .mount('#app');
