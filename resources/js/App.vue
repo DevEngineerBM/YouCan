3225da9046cacdc7b69895253055e90eb6f5bc0c
belkhalil <belkhalil1mohamed@gmail.com>
2024-05-28 21:19:09 +0100

    MIXED: Create APP.VUE EntryPoint

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+     
+    <main>
+      <router-view></router-view>
+    </main>
+    
+  </div>
+</template>
+
+<script setup>
+
+import { useRoute } from 'vue-router';
+
+
+</script>
+
+<style>
+
+</style>
